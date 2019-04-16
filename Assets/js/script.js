8f8b9ee9a0d68479742f7c8a0d614e0e35bace35
TheTrepanier <thetrepanier1@Michaels-MacBook-Pro-3.local>
2019-04-16 15:31:46 -0700

    added a javascript file to populate the portfolio page

---
+++ Assets/js/script.js
@@ -0,0 +1,57 @@
+var repoArrObj = [
+    {
+        name: "Word Guess Game",
+        repo: "https://github.com/TheTrepanier/word-guess-game",
+        liveLink: "https://thetrepanier.github.io/word-guess-game/",
+        img: "Assets/images/word-guess-game.png",
+        description: "This game of hangman written in vanilla js has a Pokemon spin! Using information I get from an Ajax call made to the PokeAPI I display a randomly selected Pokemon"
+    },
+    {
+        name: "jQuery rpg",
+        repo: "https://github.com/TheTrepanier/jQuery-rpg",
+        liveLink: "https://thetrepanier.github.io/jQuery-rpg/",
+        img: "Assets/images/jQuer-rpg.png",
+        description: "You wanna be the very best? This RPG written in jQuery is a simplified verision of the popular Pokemon games"
+    },
+    {
+        name: "Trivia Game",
+        repo: "https://github.com/TheTrepanier/trivia-game",
+        liveLink: "https://thetrepanier.github.io/trivia-game/",
+        img: "Assets/images/trivia-game.png",
+        description: "Wands away, and grab your quills! Today you take your final exams at Hogwarts School for Witchcraft and Wizardry"
+    },
+    {
+        name: "NightOut!",
+        repo: "https://github.com/TheTrepanier/first-group-project",
+        liveLink: "https://thetrepanier.github.io/first-group-project/",
+        img: "Assets/images/NightOut.png",
+        description: "NightOut! is a one stop solution to planing your own night out"
+    },
+    {
+        name: "Liri",
+        repo: "https://github.com/TheTrepanier/liri-node-app",
+        liveLink: "https://github.com/TheTrepanier/liri-node-app/blob/master/README.md",
+        img:"Assets/images/liri.gif",
+        description: "Liri is a command line tool that will take in various commands and display results from different services"
+    }
+];
+
+
+
+$(document).ready(function () {
+    for (let index = 0; index < repoArrObj.length; index++) {
+        const element = repoArrObj[index];
+        var cardImg = $("<img>").attr("src", element.img).attr("alt", element.name).addClass("card-img-top");
+        var imgLink = $("<a>").attr("href", element.liveLink).attr("target", "_blank").append(cardImg);
+        var cardTitle = $("<h5>").addClass("card-title").text(element.name);
+        var cardText = $("<p>").text(element.description);
+        var liveButton = $("<a>").addClass("btn btn-primary").attr("href", element.liveLink).attr("target", "_blank").text("Try it out");
+        var repoButton = $("<a>").addClass("btn btn-secondary ml-1").attr("href", element.repo).attr("target", "_blank").text("GitHub repo");
+        var cardBodyDiv = $("<div>").addClass("card-body").append(cardTitle, cardText, liveButton, repoButton);
+        var card = $("<div>").addClass("card").append(imgLink, cardBodyDiv);
+        
+        $("#cards-container").append(card);
+    }
+    
+});
+
